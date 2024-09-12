aec7f4cf758d120f139c5dbe96a54988596a8e01
Rupert Menneer <rupertmenneer@gmail.com>
2024-09-12 09:49:49 +0100

    renamed graph_datasets -> hig data

---
+++ hig_data/coco.py
@@ -0,0 +1,244 @@
+
+import os
+import zipfile
+import PIL
+import torchvision
+import torch
+import numpy as np
+from torch_geometric.data import HeteroData, Dataset as GeoDataset
+
+from training.dataset import Dataset
+
+try:
+    import pyspng
+except ImportError:
+    pyspng = None
+
+"""
+Loads COCO dataset from HF.
+"""
+# RUPERT MENNEER (2024)
+#----------------------------------------------------------------------------
+
+class COCOStuffDataset(Dataset):
+    
+    def __init__(self,
+        image_path,                   # Path to directory or zip for images.
+        mask_path,                    # Path to directory or zip for semantic masks.
+        resolution = None,            # Ensure specific resolution, None = anything goes.
+        **super_kwargs,               # Additional arguments for the Dataset base class.
+        ):
+
+        self._path = image_path
+        self._mask_path = mask_path
+        self._zipfiles = {}  # Store zipfile references for images and masks
+        self._cached_images = dict() # {raw_idx: np.ndarray, ...}
+        self._cached_masks = dict() # {raw_idx: np.ndarray, ...}
+
+        if self._file_ext(self._path) == '.zip':
+            self._type = 'zip'
+            self._all_fnames = {
+                'image': set(self._get_zipfile(image_path).namelist()),
+                'mask': set(self._get_zipfile(mask_path).namelist())
+            }
+            assert len(self._all_fnames['image']) == len(self._all_fnames['mask']), 'Different number of images and masks'
+        else:
+            raise IOError('Path must point to a zip')
+
+        PIL.Image.init()
+        supported_ext = PIL.Image.EXTENSION.keys() | {'.npy'}
+        unspported_prefix = ['.', '__', '__MACOSX/']
+        self._fnames = {
+            'image': sorted(fname for fname in self._all_fnames['image'] if self._file_ext(fname) in supported_ext and not any(fname.startswith(prefix) for prefix in unspported_prefix)),
+            'mask': sorted(fname for fname in self._all_fnames['mask'] if self._file_ext(fname) in supported_ext and not any(fname.startswith(prefix) for prefix in unspported_prefix)),
+        }
+        if len(self._fnames['image']) == 0:
+            raise IOError('No image files found in the specified path')
+
+        name = os.path.splitext(os.path.basename(self._path))[0]
+        raw_shape = [len(self._fnames['image'])] + list(self._load_attribute_image(0, path=self._path).shape)
+        if resolution is not None and (raw_shape[2] != resolution or raw_shape[3] != resolution):
+            raise IOError('Image files do not match the specified resolution')
+        super().__init__(name=name, raw_shape=raw_shape, **super_kwargs)
+
+    @staticmethod
+    def _file_ext(fname):
+        return os.path.splitext(fname)[1].lower()
+
+    def _get_zipfile(self, path):
+        if path not in self._zipfiles:
+            self._zipfiles[path] = zipfile.ZipFile(path)
+        return self._zipfiles[path]
+
+    def _open_file(self, fname, path):
+        if self._type == 'zip':
+            return self._get_zipfile(path).open(fname, 'r')
+        return None
+
+    def close(self):
+        try:
+            if self._zipfile is not None:
+                self._zipfile.close()
+        finally:
+            self._zipfile = None
+
+    def __getstate__(self):
+        return dict(super().__getstate__(), _zipfile=None)
+
+    def _load_image_from_path(self, fname, path): # return image from fname as np.ndarray
+        ext = self._file_ext(fname)
+        with self._open_file(fname, path) as f:
+            if ext == '.npy':
+                image = np.load(f)
+                image = image.reshape(-1, *image.shape[-2:])
+            elif ext == '.png' and pyspng is not None:
+                image = pyspng.load(f.read())
+                image = image.reshape(*image.shape[:2], -1).transpose(2, 0, 1)
+            else:
+                image = np.array(PIL.Image.open(f))
+                image = image.reshape(*image.shape[:2], -1).transpose(2, 0, 1)
+        return image
+
+    def _load_attribute_image(self, raw_idx, attribute='image', path=None):
+        image_name = self._fnames[attribute][raw_idx]
+        return self._load_image_from_path(image_name, path)
+
+        # load the preprocessed image and mask from the coco dataset
+    def __getitem__(self, idx):
+        raw_idx = self._raw_idx[idx]
+        image = self._cached_images.get(raw_idx, None)
+        mask = self._cached_masks.get(raw_idx, None)
+        if image is None:
+            image = self._load_attribute_image(raw_idx, attribute='image', path=self._path)
+            mask = self._load_attribute_image(raw_idx, attribute='mask', path=self._mask_path)
+            if self._cache:
+                self._cached_images[raw_idx] = image
+                self._cached_masks[raw_idx] = mask
+        assert isinstance(image, np.ndarray)
+        assert isinstance(mask, np.ndarray)
+        assert list(image.shape) == self._raw_shape[1:]
+        if self._xflip[idx]:
+            assert image.ndim == 3 # CHW
+            image = image[:, :, ::-1]
+            mask = mask[:, :, ::-1]
+        return image.copy(), mask.copy()
+
+
+#----------------------------------------------------------------------------
+
+"""
+Models data with a dual graph representation.
+"""
+# RUPERT MENNEER (2024)
+#----------------------------------------------------------------------------
+class CocoStuffGraphDataset(GeoDataset):
+
+    def __init__(self,
+        image_path,                   # Path to directory or zip for images.
+        mask_path,                    # Path to directory or zip for semantic masks.
+        graph_transform=None,         # Transform to apply to the graph.
+        patch_size = 8,               # Size of image patches. Set to 1 for no patches.
+        n_labels = 182,               # Number of classes in the dataset.
+    ) -> None:
+
+        super().__init__(None, graph_transform)
+        self.dataset = COCOStuffDataset(image_path, mask_path)
+        self.n_labels = n_labels
+
+        # image node positions (same for every img)
+        self.grid_size = self.dataset._raw_shape[-1] // patch_size
+        self.num_image_nodes = self.grid_size * self.grid_size
+
+        self.image_patch_positions = get_image_patch_positions(self.dataset._raw_shape[-1], patch_size)
+        
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    # construct a dual graph from raw data item 
+    def __getitem__(self, idx: int) -> HeteroData:
+
+        data = RelaxedHeteroData() # create hetero data object for dual graph
+        img, mask = self.dataset[idx]
+        data.image = torch.from_numpy(img[np.newaxis,...]) # add image to data object
+        data.mask = torch.from_numpy(mask[np.newaxis,...]) # add mask to data object
+
+        # initialise image patch nodes
+        image_patch_placeholder = torch.zeros(self.num_image_nodes, 1, dtype=torch.float32)
+        data['image_node'].x = image_patch_placeholder
+        data['image_node'].pos = self.image_patch_positions
+
+        # create class nodes from semantic segmentation map
+        self._create_class_nodes(data, mask)
+
+        # create class to image edges from semantic segmentation map
+        self._create_class_to_image_edges(data, mask)
+
+        if self.transform: # apply any additional transforms
+            data = self.transform(data)
+
+        return data
+    
+    def _create_class_nodes(self, data, mask):
+        class_labels = np.array([l for l in np.unique(mask) if l != 255], dtype=np.int16)
+        print(class_labels)
+        onehots = np.zeros((len(class_labels), self.n_labels), dtype=np.float32)
+        onehots[np.arange(len(class_labels)), class_labels] = 1
+        onehots = np.concatenate([class_labels[...,np.newaxis], onehots], axis=1) # add class labels to onehots position 0 for convenience
+        data['class_node'].x = torch.from_numpy(onehots).to(torch.float32)
+        # densely connect class nodes
+        
+        edge_index = torch.combinations(torch.arange(len(class_labels)), with_replacement=False).t()
+        edge_index = torch.cat([edge_index, edge_index.flip(0)], dim=1)
+        data['class_node', 'class_edge', 'class_node'].edge_index = edge_index
+        return data
+    
+    def _create_class_to_image_edges(self, data, mask):
+        edges = []
+        resized_mask = torch.nn.functional.interpolate(torch.tensor(mask[np.newaxis,...], dtype=torch.float32), size=(self.grid_size, self.grid_size), mode='nearest').squeeze() # resize mask to match compression
+        class_node_pos = []
+        for class_node_idx, class_label in enumerate(data['class_node'].x[:,0]):
+            class_mask = np.argwhere(resized_mask == class_label) # get mask idxs for current class
+            linear_image_patch_print_line_idxs = (class_mask[0] * self.grid_size + class_mask[1]).long() # linearise image node idxs
+
+            class_node_pos.append(linear_to_avg_2d_idx(linear_image_patch_print_line_idxs)) # get image patch positions
+
+            node_id_repeated = np.full((len(linear_image_patch_print_line_idxs),), class_node_idx, dtype=np.int16) # repeat class node idx for each patch
+            edge_index = np.stack([node_id_repeated, linear_image_patch_print_line_idxs], axis=0)
+            edges.append(edge_index)
+        class_to_image_index = np.concatenate(edges, axis=1) if edges else np.zeros((2, 0), dtype=np.int16)
+        data['class_node', 'class_to_image', 'image_node'].edge_index = torch.from_numpy(class_to_image_index) # convert to torch
+        data['class_node'].pos = torch.stack(class_node_pos, dim=0) # add class node positions for visualisation
+        return data
+    
+
+# ----------------------------------------------------------------------------
+import re
+
+class RelaxedHeteroData(HeteroData):
+    # modified to allow allow_empty flag to be passed
+    def __getattr__(self, key: str, allow_empty=True):
+        if hasattr(self._global_store, key):
+            return getattr(self._global_store, key)
+        elif bool(re.search('_dict$', key)):
+            return self.collect(key[:-5], allow_empty=allow_empty)
+        raise AttributeError(f"'{self.__class__.__name__}' has no "
+                             f"attribute '{key}'")
+    
+
+def get_image_patch_positions(image_size = 256, patch_size = 8) -> torch.Tensor:
+    """
+    Get the xy positions of each image patch in the image grid
+    """
+    grid_size = image_size // patch_size
+    grid_h = torch.arange(grid_size, dtype=torch.float32,)
+    grid_w = torch.arange(grid_size, dtype=torch.float32)
+    grid = torch.meshgrid(grid_w, grid_h, indexing='xy')  # here w goes first
+    image_patch_positions = torch.stack(grid, axis=0).flatten(1).permute(1, 0)  # (num_patches, 2) 
+    image_patch_positions *= patch_size
+    return image_patch_positions
+
+def linear_to_avg_2d_idx(idx, img_width=32, patch_size=8):
+    row = torch.mean((idx // img_width).float())*patch_size
+    col = torch.mean((idx % img_width).float())*patch_size
+    return torch.tensor([col, row])
---
+++ hig_data/visualisation.py
@@ -0,0 +1,229 @@
+
+import wandb
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import networkx as nx
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# handles batch or single image, n sets number to display
+def plot_array_images(images, n=8, save_path=None):
+    
+    if not isinstance(images, np.ndarray):
+        images = images.numpy()
+
+    if images.ndim == 2:
+        images = images[np.newaxis]
+    if images.ndim == 3:
+        images = images[np.newaxis]
+    if images.shape[0] < n:
+        n = images.shape[0]
+
+    if n == 1:
+        img = images[0].transpose(1, 2, 0)
+        plt.figure(figsize=(2, 5), dpi=300)
+        plt.imshow(img,)
+        plt.axis('off')
+
+    else:
+        _, axes = plt.subplots(1, n, figsize=(10, 10), dpi=300)
+        for i in range(n):         
+            img = images[i].transpose(1, 2, 0)
+            axes[i].imshow(img)
+            axes[i].axis('off')
+
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+@torch.no_grad()
+def logging_generate_sample_vis(
+        batch,
+        n=8,
+        labels=['HIG', 'Ground Truth', 'Sampled Image'],
+        title="Sampled Images - Validation Graphs"
+    ):
+
+    n = n if n < batch.image.shape[0] else batch.image.shape[0]
+
+    # sample images with graphs
+    # sampled_images = self.sample(init_graph_batch=batch, n=n)
+    sampled_images = torch.zeros_like(batch.image, device=batch.image.device)
+
+    # create image with ground truth graph overlay
+    graph_on_image_tensor = visualise_het_graph_on_image_batch(batch, n=n,)
+    print('graph_on_image_tensor', graph_on_image_tensor.shape)
+
+    # save to wandb
+    save_image_batch_list([graph_on_image_tensor,
+                           batch.image.numpy(),
+                           sampled_images.numpy(),],
+                        row_labels=labels,
+                        use_wandb=False,
+                        title=title,
+                        sample_batch_size=n)
+
+
+"""
+Input: list of image batches -> [[B, C, H, W], ...]
+with matching list of labels, save row wise to wandb
+"""
+def save_image_batch_list(
+        image_batch_list,
+        row_labels,
+        sample_batch_size = 8,
+        use_wandb = True,
+        vis_size_factor = 8,
+        title = "Image batch"
+    ):
+
+    cols = sample_batch_size if sample_batch_size < len(image_batch_list[0]) else len(image_batch_list[0])
+    rows = len(image_batch_list)
+
+    fig = plt.figure(figsize = (cols*vis_size_factor, rows*vis_size_factor), constrained_layout=True)
+    fig.suptitle(title,fontsize=10*vis_size_factor)
+    fig.patch.set_facecolor('white')
+
+    # create n * 1 subfigs
+    subfigs = fig.subfigures(nrows=rows, ncols=1)
+
+    for row, (subfig, image_set) in enumerate(zip(subfigs, image_batch_list)):
+        subfig.suptitle(row_labels[row], fontsize = 5*vis_size_factor)
+
+        # create 1 * batchsize subplots per subfig
+        axs = subfig.subplots(nrows=1, ncols=cols)
+        for col, (ax, ax_img) in enumerate(zip(axs, image_set)):
+            img = ax_img.transpose(1, 2, 0)
+            ax.imshow(img)
+            ax.axis('off')
+
+    if use_wandb:
+        wandb.log({f"{title}": fig})
+    else:
+        plt.show()
+
+
+def visualise_het_graph_on_image_batch(graph_batch, n=8): # unpack graph batch and return list of images
+    images = []
+    for i, graph in enumerate(graph_batch.to_data_list()):
+        if i >= n:
+            break
+        graph_on_image = visualise_het_graph_on_image(graph, return_image=True)[np.newaxis, ...]
+        images.append(graph_on_image)
+    return np.concatenate(np.array(images), axis=0)
+
+"""Visualise HIG representation, display both underlying image and graph nodes on top
+Note: hetero_data must contain node positions for each node type"""
+def visualise_het_graph_on_image(
+        hetero_data,
+        image_alpha=0.8,
+        node_types=['image_node', 'class_node'],
+        edge_types=[('class_node', 'class_to_image', 'image_node'), ('class_node', 'class_edge', 'class_node')],
+        image_size=256,
+        linewidth=1.5,
+        resize_mask=False,
+        return_image=False,
+    ):
+    # Set up the matplotlib figure
+    dpi = 1000
+    fig, ax = plt.subplots(figsize=(image_size/dpi, image_size/dpi), dpi=dpi)
+
+    # Display the image with alpha transparency
+    print(hetero_data.image.shape)
+    ax.imshow(hetero_data.image.squeeze().permute(1,2,0), alpha=image_alpha)
+
+    mask = hetero_data.mask
+    if resize_mask:
+        resized_mask = torch.nn.functional.interpolate(mask.float(), scale_factor=1/8, mode='nearest')
+        mask = torch.nn.functional.interpolate(resized_mask, scale_factor=8, mode='nearest')
+        
+    ax.imshow(mask.squeeze(), alpha=0.45)
+    
+    # Create a NetworkX graph
+    G = nx.Graph()
+
+    # Get node positions and add them to the graph
+    for node_type in node_types:
+        assert hetero_data[node_type].pos is not None, f"Node type {node_type} does not have positions"
+        node_positions = hetero_data[node_type].pos 
+        node_features = hetero_data[node_type].x
+        for i in range(node_positions.size(0)):
+
+            G.add_node(f'{node_type}_{i}', type=node_type, class_type=int(node_features[i][0]), pos=(node_positions[i, 0].item(), node_positions[i, 1].item()))
+
+    # Add edges to the graph
+    for edge_type in edge_types:
+        assert hetero_data[edge_type].edge_index is not None
+        edge_index = hetero_data[edge_type].edge_index
+        for i in range(edge_index.size(1)):
+
+            src_node = f'{edge_type[0]}_{edge_index[0, i].item()}'
+            tgt_node = f'{edge_type[2]}_{edge_index[1, i].item()}'
+
+            G.add_edge(src_node, tgt_node, type=edge_type)
+
+    # Get positions from node attributes
+    pos = nx.get_node_attributes(G, 'pos')
+    # Example usage within your main visualization function
+    node_colors = get_node_colors(G)
+
+    nodes_alphas = [0.9 if 'class_node' in n else 0. for n in G.nodes()]
+    nodes_size = [0.35 if 'class_node' in n else 0.0 for n in G.nodes()]
+    # Draw the graph on top of the image
+    nx.draw(G, pos, with_labels=False, node_size=nodes_size, alpha=nodes_alphas, node_color=node_colors, width=linewidth, ax=ax, edgelist=[])
+    
+    edges = G.edges(data=True)
+    edge_alphas = [0.5 if 'class_edge' in e[2]['type'] else 0.1 for e in edges]
+    edge_widths = [0.25 if 'class_edge' in e[2]['type'] else 0.1 for e in edges]
+    edge_styles = ['dashed' if 'class_edge' in e[2]['type'] else 'solid' for e in edges]
+    edge_colors = ['black' if 'class_edge' in e[2]['type'] else 'white' for e in edges]
+    
+    for i, (u, v, data) in enumerate(edges):        
+        nx.draw_networkx_edges(
+            G, pos,
+            edgelist=[(u, v)],
+            edge_color=edge_colors[i],
+            alpha=edge_alphas[i],
+            width=edge_widths[i],
+            style=edge_styles[i],
+        )
+
+
+    ax.set_aspect("equal")
+    ax.grid(False)
+    ax.set_xlim([0, hetero_data.image.shape[-1]])
+    ax.set_ylim([0, hetero_data.image.shape[-1]])
+    ax.axis('off') 
+    ax.set_facecolor('white')
+    fig.patch.set_facecolor('white')
+    fig.tight_layout(pad=0)
+    if return_image:
+        fig.canvas.draw()
+        w, h = fig.canvas.get_width_height()
+        image = np.frombuffer(fig.canvas.buffer_rgba(), dtype='uint8').reshape(h, w, 4)
+        pil_image = np.array(PIL.Image.fromarray(image, mode='RGBA').convert('RGB')).transpose(2, 0, 1)
+        plt.close()
+        return np.flip(pil_image, 1)
+    else:
+        plt.show()
+        plt.axis('off')
+
+
+# utility function to generate a class node color
+def generate_color_for_type(class_type, num_colors=185):
+    colormap = plt.get_cmap('gist_rainbow', num_colors)  # Using tab10 colormap for 10 distinct colors
+    return colormap(class_type)
+
+# Utility function to get node colors based on node types
+def get_node_colors(G,):
+    node_colors = []
+    for node in G.nodes(data=True):
+        if node[1]:
+            node_type = node[1]['class_type']
+            node_colors.append(generate_color_for_type(node_type,))
+    return node_colors
+
+
