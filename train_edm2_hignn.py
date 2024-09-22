3e625bf5dad9e26b3acec153f8ecebba689a639b
Rupert Menneer <rupertmenneer@gmail.com>
2024-09-22 14:14:14 +0100

    added val path + dropout to default config

--- train_edm2_hignn.py
+++ train_edm2_hignn.py
@@ -31,7 +31,7 @@ config_presets = {
     'edm2-img64-m':     dnnlib.EasyDict(duration=2048<<20, batch=2048, channels=256, lr=0.0090, decay=35000, dropout=0.10, P_mean=-0.8, P_std=1.6),
     'edm2-img64-l':     dnnlib.EasyDict(duration=1024<<20, batch=2048, channels=320, lr=0.0080, decay=35000, dropout=0.10, P_mean=-0.8, P_std=1.6),
     'edm2-img64-xl':    dnnlib.EasyDict(duration=640<<20,  batch=2048, channels=384, lr=0.0070, decay=35000, dropout=0.10, P_mean=-0.8, P_std=1.6),
-    'edm2-coco256-s':    dnnlib.EasyDict(duration=2048<<20, batch=1024, channels=192, lr=0.0100, decay=35000, dropout=0.00, P_mean=-0.4, P_std=1.0),
+    'edm2-coco256-s':    dnnlib.EasyDict(duration=2048<<20, batch=1024, channels=192, lr=0.0100, decay=35000, dropout=0.10, P_mean=-0.4, P_std=1.0),
 }
 
 #----------------------------------------------------------------------------
@@ -51,7 +51,7 @@ def setup_training_config(preset='edm2-img512-s',
             opts[key] = value
 
     # Dataset.
-    c.dataset_kwargs = dnnlib.EasyDict(class_name=dataset_name, path=opts.path,)
+    c.dataset_kwargs = dnnlib.EasyDict(class_name=dataset_name, path=opts.path, val_path=opts.val_path)
     try:
         dataset_obj = dnnlib.util.construct_class_by_name(**c.dataset_kwargs)
         dataset_channels = dataset_obj.num_channels
@@ -147,6 +147,7 @@ def parse_nimg(s):
 
 # COCO
 @click.option('--path',             help='Path to the dataset', metavar='ZIP|DIR',              type=str, required=True)
+@click.option('--val_path',         help='Path to the dataset', metavar='ZIP|DIR',              type=str, required=True)
 
 # Hyperparameters.
 @click.option('--duration',         help='Training duration', metavar='NIMG',                   type=parse_nimg, default=None)
@@ -167,7 +168,7 @@ def parse_nimg(s):
 # I/O-related options.
 @click.option('--status',           help='Interval of status prints', metavar='NIMG',           type=parse_nimg, default='128Ki', show_default=True)
 @click.option('--snapshot',         help='Interval of network snapshots', metavar='NIMG',       type=parse_nimg, default='8Mi', show_default=True)
-@click.option('--checkpoint',       help='Interval of training checkpoints', metavar='NIMG',    type=parse_nimg, default='128Mi', show_default=True)
+@click.option('--checkpoint',       help='Interval of training checkpoints', metavar='NIMG',    type=parse_nimg, default='32Mi', show_default=True)
 @click.option('--wandb_vis',       help='Interval of wandb vis', metavar='NIMG',                type=parse_nimg, default='1Mi', show_default=True)
 @click.option('--seed',             help='Random seed', metavar='INT',                          type=int, default=0, show_default=True)
 @click.option('-n', '--dry-run',    help='Print training options and exit',                     is_flag=True)
