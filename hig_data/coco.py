6f48c1ed809bfc048e9a1f11b6f6d6baaff827f5
Rupert Menneer <rupertmenneer@gmail.com>
2024-10-14 12:41:37 +1100

    graph edge updates: instances now connected to class and dense class connections are the only two connections. Will implement more sophisicated strategy at some point

--- hig_data/coco.py
+++ hig_data/coco.py
@@ -353,10 +353,11 @@ class COCOStuffGraphPrecomputedDataset(GeoDataset):
             # data['caption_node'].x = data.caption if data.caption.shape[0] != 0 else torch.empty((0, 768))
 
             # ---- Edges
-            data['class_node', 'class_edge', 'class_node'].edge_index = torch.from_numpy(graph_group['class_edge'][:]).to(torch.long) if graph_group['class_edge'][:].shape[1] != 0 else torch.empty((2, 0), dtype=torch.long)
             data = self.connect_instance_and_class(data)
+            data['class_node', 'class_edge', 'class_node'].edge_index = torch.from_numpy(graph_group['class_edge'][:]).to(torch.long) if graph_group['class_edge'][:].shape[1] != 0 else torch.empty((2, 0), dtype=torch.long)
+            
             # data = self.connect_with_clip_similarity(data)
-            data['instance_node', 'instance_edge', 'instance_node'].edge_index = torch.from_numpy(graph_group['instance_edge'][:]).to(torch.long) if graph_group['instance_edge'][:].shape[1] != 0 else torch.empty((2, 0), dtype=torch.long)
+            # data['instance_node', 'instance_edge', 'instance_node'].edge_index = torch.from_numpy(graph_group['instance_edge'][:]).to(torch.long) if graph_group['instance_edge'][:].shape[1] != 0 else torch.empty((2, 0), dtype=torch.long)
 
             # class to image
             data['class_node', 'class_to_image', 'image_node'].edge_index = torch.from_numpy(graph_group['class_to_image'][:]).to(torch.long) if graph_group['class_to_image'][:].shape[1] != 0 else torch.empty((2, 0), dtype=torch.long)
